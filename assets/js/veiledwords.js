751af9a41cb846efb42cf9822614b96be433a6d5
Tara <tararafi@gmail.com>
2020-08-22 22:07:59 -0400

    changed wording on about

--- assets/js/veiledwords.js
+++ assets/js/veiledwords.js
@@ -4,7 +4,7 @@ document.getElementById("veilTitle").onclick =  function unveil(){
         document.getElementById("veilTitle").innerHTML = "Veil"
     }
     else{
-        document.getElementById("trueWords").innerHTML = "We strive to help domestic abuse victims find resources and communties that bring them closer to a happier lifestyle. COVID - 19 has made domestic abuse situations so much worse and we are here to help. Enter your location to find resources close to you or browse through more general resources. You are not alone at Veil. ";
+        document.getElementById("trueWords").innerHTML = "We strive to help domestic abuse victims find resources and communties that can bring them closer to a happier life. With COVID - 19 endangering many women with new quarantine regulations, Veil has created a platform to discretely find help online, from your home. Find shelters close to you with our interactive map under Find Stores, or browse through more general resources in the shop section. You are not alone at Veil. ";
         document.getElementById("veilTitle").innerHTML = "UnVeil"
     }
 };
